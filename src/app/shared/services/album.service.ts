c7ec7cf0f730cfd2c906eba98e16193b3428d162
PavloZaluskyy <pavlozaluskiy@gmail.com>
2021-02-25 17:31:33 +0200

    May fixed get request bag

--- src/app/shared/services/album.service.ts
+++ src/app/shared/services/album.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError  } from 'rxjs';
 import { Album } from "../interfaces/album";
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
-  private ATOKEN = '22e5dcb7293a23da484afeacce80c247'
+  private ATOKEN = '05dd98f8d26294ae45ab788a24a12bdb'
   constructor(private http: HttpClient) { }
 
   getAlbum(genre) :Observable<Album[]>{
     const url:string = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=${genre}&api_key=${this.ATOKEN}&format=json`;
-    return this.http.get<Album[]>(url);
+    return this.http.get<Album[]>(url,
+      {
+        headers:
+          new HttpHeaders(
+            {
+              'Content-Type': 'application/json',
+              'X-Requested-With': 'XMLHttpRequest',
+              'MyClientCert': '',        // This is empty
+              'MyToken': ''              // This is empty
+            }
+          )
+      }
+    ).pipe( map(res => res), catchError(err => throwError(err)) );
   }
 }
